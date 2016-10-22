caa1008f856e61ff0b7400ab275650b5eb9ee27b
https://github.com/new <bmcordero@abany.edu>
2016-10-21 21:30:01 -0400

    Changes node contructers to take in a String variable

--- Node.java
+++ Node.java
@@ -1,13 +1,36 @@
  public class Node {
   // properties
-  private int data;
+  private String data;
   private Node prev;
   private Node next;
 
   // Constructors
-  Node(int d) { 
+  Node(String d) { 
     this.data = d; 
     this.next = null;
     this.prev = null;
+  }
+  public String getData(){
+    return this.data;
+  }
+  
+  public Node getPrev(){
+    return this.prev;
+  }
+  
+  public Node getNext() {
+    return this.next;
+  }
+  
+  public void setData(String newData) {
+    this.data = newData;
+  }
+  
+  public void setPrev(Node newPrev) {
+    this.prev = newPrev;
+  }
+  
+  public void setNext(Node newNext) {
+    this.next = newNext;
   }
  }
