b4424f36d890cc14b66ad3269b58ac60b0a8235c
https://github.com/new <bmcordero@abany.edu>
2016-10-21 21:11:41 -0400

    creates getters and setters for doubly linked list

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -9,6 +9,23 @@ public class DoublyLinkedList
     this.head = null;
     this.tail = null;
   }
+  // getters and setters
+   public Node getHead() {
+    return this.head;
+  }
+  public Node getTail() {
+    return this.tail;
+  }
+  public void setHead(Node newHead) {
+    if (this.head == this.tail)
+      this.tail = newHead;
+    this.head = newHead;
+  }
+  public void setTail(Node newTail) {
+    if (this.head == this.tail)
+      this.head = newTail;
+    this.tail = newTail;
+  }
   
 }
  
