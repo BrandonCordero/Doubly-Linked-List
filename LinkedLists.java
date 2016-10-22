83710be63a4bc1f25f7f0c7c8bc68414353e9993
https://github.com/new <bmcordero@abany.edu>
2016-10-21 21:06:40 -0400

    Creates main for doubly linked list assignment

---
+++ LinkedLists.java
@@ -0,0 +1,18 @@
+public class LinkedLists {
+  public static void main (String [] args) {
+    DoublyLinkedList myList = new DoublyLinkedList();
+  }
+  
+}
+    
+   /* myList.addData("Brandon");
+    myList.addData("Irene");
+    myList.addData("Martin");
+    myList.addData("Emily");
+    myList.addData("Alec");
+    
+    myList.print();
+    
+  }
+}
+*/
