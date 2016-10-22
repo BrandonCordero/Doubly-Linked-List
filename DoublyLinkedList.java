6d1dcbcf41cf98b9017fee3941a0653b3360c79b
https://github.com/new <bmcordero@abany.edu>
2016-10-21 21:02:28 -0400

    Creates properties and Constructors for Doubly Linked List

---
+++ DoublyLinkedList.java
@@ -0,0 +1,16 @@
+public class DoublyLinkedList
+{
+  // properties
+  Node head;
+  Node tail;
+
+  // Constructors
+  DoublyLinkedList() { 
+    this.head = null;
+    this.tail = null;
+  }
+  
+}
+ 
+ 
+  
