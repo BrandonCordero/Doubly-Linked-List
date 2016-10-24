58e6710bb2442b81f204e69a5887fade218fe406
https://github.com/new <bmcordero@abany.edu>
2016-10-23 20:03:10 -0400

    Adds print statements to code

--- LinkedLists.java
+++ LinkedLists.java
@@ -7,9 +7,12 @@ public class LinkedLists {
     myList.addData("Martin");
     myList.addData("Emily");
     myList.addData("Alec");
+    System.out.println("Current List");
     myList.print();
+    System.out.println("List in reverse");
     myList.printReverse();
-    myList.deleteNode("Emily");
+    System.out.println(" Deletes node from list");
+     myList.deleteNode("Emily");
     myList.print();
   }
 }
